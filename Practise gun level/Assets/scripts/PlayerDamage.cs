db8c8ae05bf47332a36e82bf4df0d8e1e9d61ed8
Joseph <124595448+RockyJoe2001@users.noreply.github.com>
2023-06-05 15:53:23 +0100

    change

--- Practise gun level/Assets/scripts/PlayerDamage.cs
+++ Practise gun level/Assets/scripts/PlayerDamage.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class PlayerDamage : MonoBehaviour
 {
+    // CONDITION: When the projectile hits a certain object type... (enemy)
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        // Check if the object we collided with has the tag we are looking for (Enemy)
+        if (otherCollider.CompareTag("Enemy") == true)
+        {
+            // Perform our action
+            KillEnemy(otherCollider.gameObject);
+        }
+    }
+
+
+    // ACTION: Destroy an object (enemy)
+    public void KillEnemy(GameObject enemy)
+    {
+        Destroy(enemy);
+    }
     // Start is called before the first frame update
     void Start()
     {
