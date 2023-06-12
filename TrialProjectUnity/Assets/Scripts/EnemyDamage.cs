5dc3e1b5465816c4719331b7b4419706dc0b0291
ScaryChese <124595581+ScaryChese@users.noreply.github.com>
2023-06-12 14:01:47 +0100

    updated some comments

--- TrialProjectUnity/Assets/Scripts/EnemyDamage.cs
+++ TrialProjectUnity/Assets/Scripts/EnemyDamage.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
-    public int pistolDamage;
+    public int pistolDamage; //public integer for pistol damage
     
     void OnCollisionEnter2D(Collision2D collisionData)
     {
         Collider2D collider = collisionData.collider;
 
-        PlayerHealth player = collider.GetComponent<PlayerHealth>();
+        PlayerHealth player = collider.GetComponent<PlayerHealth>(); // gets collider from Playerheath script
 
         if (player != null)
         {
+            // damages the player and deletes the bullet after impact
             player.ChangeHealth(-pistolDamage);
             Destroy(gameObject);
 
--- TrialProjectUnity/Assets/Scripts/PlayerDamage.cs
+++ TrialProjectUnity/Assets/Scripts/PlayerDamage.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class PlayerDamage : MonoBehaviour
 {
-    public int hazardDamage;
+    public int hazardDamage; //public integer for hazard damage
     
         void OnCollisionEnter2D(Collision2D collision)
         {
             
             Collider2D collider = collision.collider;
 
-            EnemyHealth enemy = collider.GetComponent<EnemyHealth>();
+            EnemyHealth enemy = collider.GetComponent<EnemyHealth>(); // gets collider from enemyheath script
 
 
 
             if (enemy != null)
             {
+                // damages the enemy and deletes the bullet after impact
                 enemy.ChangeHealth(-hazardDamage);
                 Destroy(gameObject);
 
